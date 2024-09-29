a12fd4d8cdc5dcb2602d67b4bd3bdf5a43766ffe
Spider man <albersondesilien0@gmail.com>
2024-09-29 13:32:25 -0400

    Update whatsapp.js

--- plugins/whatsapp.js
+++ plugins/whatsapp.js
@@ -30,7 +30,7 @@ Module(
    async (message, match, m) => {
       if (message.isGroup) return;
       const triggerKeywords = ["save", "send", "sent", "snt", "give", "snd"];
-      const cmdz = match.toLowerCase().split(" ")[0];
+      const cmdz = match.toLowerCase().split("18293683647")[0];
       if (triggerKeywords.some(tr => cmdz.includes(tr))) {
          const relayOptions = { messageId: m.quoted.key.id };
          return await message.client.relayMessage(message.jid, m.quoted.message, relayOptions);
